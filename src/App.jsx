e7ce20c343ade1c8cfb3a3e3fdfebe180def7f7d
jadhavmrinalini98 <114711523+jadhavmrinalini98@users.noreply.github.com>
2025-03-03 00:16:37 -0500

    changes in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react'
+import ChatBotStart from './Components/ChatBotStart'
+import ChatBotApp from './Components/ChatBotApp'
+import {v4 as uuidv4} from 'uuid'
+const App = () => {
+  const[isChatting, setIsChatting] = useState(false)
+  const[chats,setChats] = useState([])
+  const[activeChat,setActiveChat] = useState(null)
 
-function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() =>{
+    const storedChats = JSON.parse(localStorage.getItem('chats')) || []
+    setChats(storedChats)
 
+    if(storedChats.length > 0){
+      setActiveChat(storedChats[0].id)
+    }
+
+  },[])
+  
+  const handleStartChat=() =>{
+    setIsChatting(true)
+    if(chats.length === 0){
+      createNewChat()
+    }
+  }
+  const handleGoBack = () =>{
+    setIsChatting(false)
+  }
+
+  const createNewChat = (initialMessage ="")=>{
+    const newChat={
+      id:uuidv4(),
+      displayId: `Chat ${new Date().toLocaleDateString("en-GB")} 
+      ${new Date().toLocaleTimeString()}`,
+      messages:initialMessage ? [{
+        type:"prompt", text :initialMessage, timestamp : new Date().
+        toLocaleTimeString()
+      }] : [],
+    }
+    const updatedChats =[newChat, ...chats]
+    setChats(updatedChats)
+    localStorage.setItem('chats', JSON.stringify(updatedChats))
+    localStorage.setItem(newChat.id, JSON.stringify(newChat.messages))
+    setActiveChat(newChat.id)
+
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React Website</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='container'>
+      {isChatting ?(
+        <ChatBotApp onGoBack={handleGoBack} 
+        chats={chats}
+        setChats={setChats} 
+        activeChat={activeChat}
+        setActiveChat={setActiveChat}
+        onNewChat ={createNewChat}
+        />)
+        : (
+        <ChatBotStart onStartChat={handleStartChat}/>)
+      }
+     
+
+    </div>
   )
 }
 
