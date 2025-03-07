ff5c649d0edf38da36c9ee02c02484f4f9e2ec4b
prajwalVermaNEU <144559798+prajwalVermaNEU@users.noreply.github.com>
2025-03-06 22:25:49 -0500

    Update api-key.js

--- src/api-key.js
+++ src/api-key.js
@@ -1,4 +1,5 @@
 export async function onRequest(context) {
+    conosle.log("PRajwal reached here too");
     return new Response(import.meta.env.VITE_OPENAI_API_KEY, {
         headers: { 'Content-Type': 'text/plain' },
     });
